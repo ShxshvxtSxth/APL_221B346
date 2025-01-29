f7fd8ba7c8363519bb4401f482ac26afac51c1f9
Shashvat <shashvatseth03@gmail.com>
2025-01-29 15:16:52 +0530

    changes

--- lab2/problem2/Main (1).java
+++ lab2/problem2/Main (1).java
@@ -10,7 +10,7 @@ public class Main{
     public static void main(String[] args){
         Voice voice = new Voice();
         voice.prepareVoice();
-        voice.hear();
+        //voice.hear();
     }
 }
 
--- lab2/problem2/voice.java
+++ lab2/problem2/voice.java
@@ -7,9 +7,10 @@ class Voice{
         animal[2]=new Lion();
         animal[3]=new Tiger();
         animal[4]=new Pig();
+        hear(animal);
     }
     
-    void hear(){
+    void hear(Animal[] animal){
         for(int i=0;i<5;i++){
             animal[i].makeVoice();
         }
