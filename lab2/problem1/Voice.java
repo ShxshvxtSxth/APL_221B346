e3432ae66547550c776d4858b89bd624f64af756
Shashvat <shashvatseth03@gmail.com>
2025-01-29 15:18:56 +0530

    changes

---
+++ lab2/problem1/Voice.java
@@ -0,0 +1,15 @@
+class Voice{
+    void prepareVoice(){
+        Animal[] animal=new Animal[5];
+        animal[0]=new Cow();
+        animal[0]=new Goat();
+        animal[0]=new Lion();
+        animal[0]=new Tiger();
+        animal[0]=new Pig();
+    }
+    void hear(){
+        for(int i=0;i<5;i++){
+            animal[i].makeVoice();
+        }
+    }
+}
